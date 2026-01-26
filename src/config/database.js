aa8d6ef1e670c5178a24ea93aca43059867024e9
shivam <shivam.applicant@gmail.com>
2026-01-26 15:19:35 +0530

    DB connected successfully

--- src/config/database.js
+++ src/config/database.js
@@ -14,7 +14,7 @@ const URI = "mongodb+srv://sg491919_db_user:bjFvoW7INyf7S6gJ@cluster0.j9w2izz.mo
 
 
 const connectDB = async() =>{
-    const connectDB = await mongoose.connect(URI);
+    await mongoose.connect(URI);
 }
 
 module.exports = connectDB
